5e6d5701bd2f5fc11d0d82143c258f5490928e6e
Randall Zaitz <randallzaitz@gmail.com>
2014-04-14 17:41:19 -0500

    adding server side logic

--- server.js
+++ server.js
@@ -60,3 +60,7 @@ var server = http.createServer(function(request, response) {
 server.listen(3000, function() {
     console.log("Server listening on port 3000.");
 })
+
+// start up Socket.io server side functionality
+var chatServer = require('./lib/chat_server');
+chatServer.listen(server);
